50f46a44cf331d874f353480a2d8bb6dea974d53
bgfxc4 <bindermarc@gmx.de>
2022-02-21 22:54:45 +0100

    added status text

--- js/index.js
+++ js/index.js
@@ -1,28 +1,32 @@
-var socket = new WebSocket("ws://192.168.178.97:5555")
+var socket = new WebSocket("ws://localhost:5555")
 
-document.addEventListener("keydown", event => {
-	if (event.repeat)
-		return
+socket.onopen = event => {
+	document.addEventListener("keydown", event => {
+		if (event.repeat)
+			return
 
-	if (event.key == "w") {
-		socket.send("speed 1")
-	} else if (event.key == "s") {
-		socket.send("speed -1")
-	} else if (event.key == "a") {
-		socket.send("direction l")
-	} else if (event.key == "d") {
-		socket.send("direction r")
-	}
-})
+		if (event.key == "w") {
+			socket.send("speed 1")
+		} else if (event.key == "s") {
+			socket.send("speed -1")
+		} else if (event.key == "a") {
+			socket.send("direction l")
+		} else if (event.key == "d") {
+			socket.send("direction r")
+		}
+	})
 
-document.addEventListener("keyup", event => {
-	if (event.key == "w") {
-		socket.send("speed 0")
-	} else if (event.key == "s") {
-		socket.send("speed 0")
-	} else if (event.key == "a") {
-		socket.send("direction n")
-	} else if (event.key == "d") {
-		socket.send("direction n")
-	}
-})
+	document.addEventListener("keyup", event => {
+		if (event.key == "w") {
+			socket.send("speed 0")
+		} else if (event.key == "s") {
+			socket.send("speed 0")
+		} else if (event.key == "a") {
+			socket.send("direction n")
+		} else if (event.key == "d") {
+			socket.send("direction n")
+		}
+	})
+	$('#status-txt').toggleClass(['error-txt', 'confirm-txt']).html("Connected!")
+}
+
